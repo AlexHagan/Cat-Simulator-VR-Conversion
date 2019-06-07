5a06e62e60b2ea739c72a0ba4db38859e1157d38
AlexHagan <catsim@DESKTOP-DEMICH5.localdomain>
2019-06-06 20:41:54 -0500

    User interface now responds to controller input using the HTC Vive Input Utility from the asset store.

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void buttonClick()
+    {
+    	Debug.Log("Button clicked!");
+
+    }
+}
